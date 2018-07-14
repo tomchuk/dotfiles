6be92a5249dd50dd5fdad189b298155d1e415bde
Thomas Achtemichuk <tom@tomchuk.com>
2018-07-14 15:17:05 -0400

    updates

--- brew.sh
+++ brew.sh
@@ -1,71 +1,59 @@
-#!/usr/bin/env bash
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-# Install command-line tools using Homebrew.
+brew updateq
 
-# Ask for the administrator password upfront.
-sudo -v
+# thomas ALL=(ALL) NOPASSWD: ALL /etc/sudoers
 
-# Keep-alive: update existing `sudo` time stamp until the script has finished.
-while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
-# Make sure we’re using the latest Homebrew.
-brew update
+brew cask install iterm2-nightly
+brew install zsh zsh-completions
+brew install vim --with-override-system-vi
+if ! fgrep -q '/usr/local/bin/zsh' /etc/shells; then
+  echo '/usr/local/bin/zsh' | sudo tee -a /etc/shells;
+fi;
+sudo chpass -s /usr/local/bin/zsh thomas
 
-# Upgrade any already-installed formulae.
-brew upgrade --all
-
-# Install GNU core utilities (those that come with OS X are outdated).
-# Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
-brew install coreutils
-sudo ln -s /usr/local/bin/gsha256sum /usr/local/bin/sha256sum
-
-# Install some other useful utilities like `sponge`.
-brew install moreutils
-# Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed.
-brew install findutils
-# Install GNU `sed`, overwriting the built-in `sed`.
+brew install coreutils moreutils findutils dark-mode speedtest_cli
 brew install gnu-sed --with-default-names
-# Install Bash 4.
-# Note: don’t forget to add `/usr/local/bin/bash` to `/etc/shells` before
-# running `chsh`.
-brew install bash
-brew tap homebrew/versions
-brew install bash-completion2
+brew install libpng libtiff libxml2 libjpeg wget tree htop gnupg
+brew install awscli curl git git-lfs gpg-agent mas node the_silver_searcher watchman
+brew install python mercurial git nginx rabbitmq mysql@5.7 redis elasticsearch@1.3
 
-# Install `wget` with IRI support.
-brew install wget --with-iri
+brew cask install appcleaner transmit vlc shimo little-snitch micro-snitch keybase paw handbrake
+brew cask install bee backblaze bartender homebrew/cask-versions/firefox-developer-edition steam
+brew cask install google-chrome intensify-pro kaleidoscope muzzle tableplus transmission-nightly
 
-# Install more recent versions of some OS X tools.
-brew install vim --override-system-vi
-brew install homebrew/dupes/grep
-brew install homebrew/dupes/openssh
-brew install homebrew/dupes/screen
-brew install python
+pip2 install hg-evolve
+pip3 install flake8
 
-# Install font tools.
-brew tap bramstein/webfonttools
-brew install sfnt2woff
-brew install sfnt2woff-zopfli
-brew install woff2
+mas install 413965349  #  Soulver
+mas install 824183456  #  Affinity Photo
+mas install 824171161  #  Affinity Designer
+mas install 985614903  #  SQLPro Studio
+mas install 890031187  #  Marked 2
+mas install 411643860  #  DaisyDisk
+mas install 1091189122 #  Bear
+mas install 424390742  #  Compressor
+mas install 777874532  #  Cinemagraph Pro
+mas install 937984704  #  Amphetamine
+mas install 1290358394 #  Cardhop
+mas install 1333542190 #  1Password 7
+mas install 874920950  #  Proxy
+mas install 990588172  #  Gestimer
+mas install 441258766  #  Magnet
+mas install 638332853  #  Logitech Camera
+mas install 975937182  #  Fantastical 2
+mas install 924726344  #  Deliveries
+mas install 461369673  #  VOX
+mas install 431224317  #  Disk Drill
+mas install 424389933  #  Final Cut
+mas install 1312996177 #  Photolemur
+mas install 1081413713 #  GIF Brewery
+mas install 413535631  #  SignalScope Pro
+mas install 1035350363 #  Teeny Tokyo
+mas install 425424353  #  The Unarchiver
+mas install 803453959  #  Slack
+mas install 980406518  #  iPull
+mas install 1262957439 #  Textual 7
+mas install 576338668  #  Leaf
 
-# Install other useful binaries.
-brew install ack
-brew install dark-mode
-brew install git
-brew install git-lfs
-brew install imagemagick --with-webp
-brew install lua
-brew install lynx
-brew install p7zip
-brew install pigz
-brew install pv
-brew install rename
-brew install rhino
-brew install speedtest_cli
-brew install ssh-copy-id
-brew install tree
-brew install webkit2png
-brew install zopfli
-
-# Remove outdated versions from the cellar.
-brew cleanup
